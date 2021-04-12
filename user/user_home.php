b712c02f1a938d2933389e31295972aec3a42053
Sarthak0011 <sarthaksolanki11@gmail.com>
2021-04-12 14:54:35 +0530

    done

--- user/user_home.php
+++ user/user_home.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Bootstrap 4 Sidebar Menu Responsive with Sub menu Create Responsive Side Navigation</title>
+    <title>User home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
