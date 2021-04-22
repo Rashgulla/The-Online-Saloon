d1a776770540eb15f5cca6c55449e94e666d7fb7
Sarthak0011 <sarthaksolanki11@gmail.com>
2021-04-22 15:17:33 +0530

    ok

---
+++ Home/user_reg_con.php
@@ -0,0 +1,38 @@
+<?php
+
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
+    $mobile = $_POST['mobile'];
+    $hnumber = $_POST['hnumber'];
+    $society = $_POST['society'];
+    $landmark = $_POST['landmark'];
+    $city = $_POST['city'];
+    $pin = $_POST['pin'];
+
+    //Database connection
+    $host="localhost";
+    $user="root";
+    $password="";
+    $dbname="theonlinesalon";
+    
+    $conn = mysqli_connect($host,$user,$password,$dbname);
+    if(!$conn){
+        die("NO connection :".mysqli_connect_error());
+    }
+    else{
+        echo "OK<br>";
+    }
+
+    $sql = INSERT INTO `user_registration` (`email`, `username`, `password`, `cpassword`, `mobile`, `hnumber`, `society`, `landmark`, `city`, `pin`) 
+    VALUES ('$email', '$username',' $password', '$cpassword', '$mobile', '$hnumber', '$society', '$landmark', '$city', '$pin');
+    $result = mysqli_query($conn,$sql);
+
+    if($result){
+        echo "account created";
+    }
+    else{
+        echo "account not created".mysqli_error($conn);
+    }
+?>
