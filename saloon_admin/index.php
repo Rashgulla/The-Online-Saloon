35ee66190cc441cd3bf66d27ec2be6a908d4c9cc
Rashgulla <boogey7410@gmail.com>
2021-04-29 02:19:15 +0530

    b

--- saloon_admin/index.php
+++ saloon_admin/index.php
@@ -27,13 +27,11 @@ include('includes/navbar.php');
               <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"> saloon appointment</div>
               <div class="h5 mb-0 font-weight-bold text-gray-800">
 
-               <h4>SCHEDULE</h4>
+               <h4>View appointment</h4>
 
               </div>
             </div>
-            <div class="col-auto">
-              <i class="fas fa-calendar fa-2x text-gray-300"></i>
-            </div>
+           
           </div>
         </div>
       </div>
@@ -45,11 +43,11 @@ include('includes/navbar.php');
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">NEW APPOINTMENT</div>
+              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total APPOINTMENT</div>
               <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
             </div>
             <div class="col-auto">
-              <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+              
             </div>
           </div>
         </div>
@@ -58,23 +56,6 @@ include('includes/navbar.php');
 
     
 
-    <!-- Pending Requests Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-warning shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending appointment</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">3</div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-comments fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
   <!-- Content Row -->
 
