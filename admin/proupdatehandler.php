57ea0bb1e85a010ca3c025269a209839e7730b08
Sarthak0011 <sarthaksolanki11@gmail.com>
2021-04-29 12:16:27 +0530

    ok

--- admin/proupdatehandler.php
+++ admin/proupdatehandler.php
@@ -8,16 +8,20 @@ if(isset($_POST['update']))
     $newdesc=$_POST['description'];
 
     
-    $target = "../admin/uploads/";
+    /*$target = "../admin/uploads/";
     $file_path = $target.basename($_FILES['picture']['name']);
     $file_name = $_FILES['picture']['name'];
     $file_tmp = $_FILES['picture']['tmp_name'];
     $file_store = "../admin/uploads/".$file_name;
 
-    move_uploaded_file($file_tmp, $file_store);
+    move_uploaded_file($file_tmp, $file_store);*/
+
+    $cimage = $_FILES['picture']['name'];
+	$cimage_tmp = $_FILES['picture']['tmp_name'];
+    move_uploaded_file($cimage_tmp,"uploads/$cimage");
 
 
-    $sql="UPDATE products SET name='$newname', price='$newprice', description='$newdesc', picture='$file_path' WHERE id='$newid'";
+    $sql="UPDATE products SET name='$newname', price='$newprice', description='$newdesc', picture='$cimage' WHERE id='$newid'";
 
     if(mysqli_query($conn,$sql)){
         header('location: productshow.php');
