844f5a4fd3a5aa714eda245007a3e7f2b9e47191
Rashgulla <boogey7410@gmail.com>
2021-04-30 17:33:59 +0530

    hh

--- user/salon_details.php
+++ user/salon_details.php
@@ -1,89 +1,197 @@
 <?php
 session_start();
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THE ONLINE SALOON || Saloon-details</title>
+
+    <!-- owl carousel css file cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="../css/pstyle.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="user_home.css">
-    <title>Get your appointment here</title>
-  </head>
-  <body>
+</head>
 
-    <!--slider-->
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">    
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
+<body>
+
+    <!-- header section starts  -->
+
+
+    <header>
+
+        <div class="header-1">
+
+            <a href="#" class="logo" img src="tgb/logo"> <i class="fas fa-shopping-bag"></i> THE ONLINE SALOON </a>
+
+            <div class="form-container">
+                <form action="">
+    
+                </form>
+            </div>
+
+        </div>
+
+        <div class="header-2">
+
+            <div id="menu" class="fas fa-bars"></div>
+
+            <nav class="navbar">
+                <ul>
+                    <li><a class="active" href="#home">home</a></li>
+                    <li><a href="product.php">products</a></li>
+
+
+                </ul>
+            </nav>
+
+            <div class="icons">
+\
+                <a href="userprofile.php" class="fas fa-user"></a>
+            </div>
+
+        </div>
+
+    </header>
+
+    <!-- header section ends -->
+
+    <!-- home section starts  -->
+
+
+    <!-- <div class="container">-->
+    <!--<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="../images/shop1.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Appointment</h5>
-                    <p>Now book your appointment by sitting there.</p>
-                    
-                </div>
+            <div>
+                <img src="tgb/slide1.jpg" class="d-block w-100" alt="..." style="height: 500px;">
             </div>
-            <div class="carousel-item">
-                <img src="../images/shop2.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Products</h5>
-                    <p>Get our products online</p>
+
+        </div>
+    </div>-->
+    <!--</div>-->
+    <!-- home section ends -->
+
+    <!-- arrival section starts  -->
+
+    <section class="arrival" id="arrival">
+
+
+        <section class="content">
+            <div class="container-fluid d-flex justify-content-center">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-sm-9">
+
+
+                    <img src="../images/bg-img4.jpg" class="mt-4" alt="" style="height: 250px; width: 250px;"><br>
+                    <h3><label class="mt-4" style="color: red; font-weight:bold;">Saloon Name :</label><br> </h3>
+                    <hr><br>
+                    <h3><label class="mt-4" style="color: red; font-weight:bold;">Mobile No.</label><br> </h3>
+                    <hr><br>
+         <!-- Example single danger button -->
+            <h3>
+                 <div class="btn-group">
+                     <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Services
+                    </button>
+                     <div class="dropdown-menu">
+                        <a class="dropdown-item" > <div class="form-check">
+                             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                          <label class="form-check-label" for="flexCheckDefault">
+                           Hair cut  
+                          </label>
+                          </div></a>
+                        <a class="dropdown-item"> 
+                        <div class="form-check">
+                             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" >
+                          <label class="form-check-label" for="flexCheckDefault">
+                           Hair Wash 
+                          </label>
+                          </div></a>
+                        <a class="dropdown-item">
+                        <div class="form-check">
+                             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" >
+                          <label class="form-check-label" for="flexCheckDefault">
+                           Hair color
+                          </label>
+                          </div>
+                        </a>
+                     <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Beard Care</a>
+                        <a class="dropdown-item">
+                        <div class="form-check">
+                             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" >
+                          <label class="form-check-label" for="flexCheckDefault">
+                           Beard Cut
+                          </label>
+                          </div>
+                        </a>
+                        <a class="dropdown-item">
+                        <div class="form-check">
+                             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" >
+                          <label class="form-check-label" for="flexCheckDefault">
+                           Beard color
+                          </label>
+                          </div>
+                        </a>
+                     
+                 </div> <bR> </h3>
+                   <hr> <br>
+                
                    
+                    <h3><label class="mt-4" style="color: red; font-weight:bold;">Address</label><br> </h3>
+                    <hr><br>
+                    
+                   
+                    <button type="button" class="btn btn-success mt-3" style="font-size:18px;">Book Appointment</button>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src="../images/shop3.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Register your shop</h5>
-                    <p>Register your shop to get online customers at your shop</p>
-                    <button class="btn btn-light">Register shop</button>
-                </div>
             </div>
+        </section>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-        
-    </div>
 
-    <!--details-->
-    <div class="card">
-        <div class="card-header">
-            Featured
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Shop name</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    -->
-  
-  
-  
-  </body>
+
+
+
+
+
+    </section>
+
+    <!-- arrival section ends -->
+
+
+
+
+
+
+    <!--jquery cdn link-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="js/qwl.carousel.min.js"></script>
+
+    <!-- custom js file link  -->
+    <script src="js/main.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="user_home.js"></script>
+
+</body>
+
 </html>
