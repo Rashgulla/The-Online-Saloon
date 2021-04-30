b65263f427ef19a06636f2cb1d3456b4f974f8d8
Sarthak0011 <sarthaksolanki11@gmail.com>
2021-04-30 19:53:50 +0530

    okk

--- saloon_admin/register.php
+++ saloon_admin/register.php
@@ -1,6 +1,7 @@
 <?php
-include('includes/header.php'); 
-include('includes/navbar.php'); 
+session_start();
+include('includes/header.php');
+include('includes/navbar.php');
 ?>
 
 
@@ -17,44 +18,44 @@ include('includes/navbar.php');
 
         <div class="modal-body">
 
-        <div class="form-group">
-                <label> E-Mail</label>
-                <input type="text" name="email" class="form-control" placeholder="Enter email address">
-            </div>
+          <div class="form-group">
+            <label> E-Mail</label>
+            <input type="text" name="email" class="form-control" placeholder="Enter email address">
+          </div>
 
-            <div class="form-group">
-                <label> Username </label>
-                <input type="text" name="username" class="form-control" placeholder="Enter Username">
-            </div>
-           
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" placeholder="Enter Password">
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirmpassword" class="form-control" placeholder="Confirm Password">
-            </div>
-            
-            <div class="form-group">
-                <label>Mobile No.  </label>
-                <input type="number" name="Mobile No." class="form-control" placeholder="Enter Mobile No.">
-            </div>
+          <div class="form-group">
+            <label> Username </label>
+            <input type="text" name="username" class="form-control" placeholder="Enter Username">
+          </div>
 
-            <div class="form-group">
-                <label> Saloon picture </label>
-                <input type="file" name="picture" class="form-control" placeholder="Chosee Picture">
-            </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" placeholder="Enter Password">
+          </div>
+          <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="password" name="confirmpassword" class="form-control" placeholder="Confirm Password">
+          </div>
 
-            <div class="form-group">
-                <label> Address </label>
-                <input type="text" name="Address" class="form-control" placeholder="Enter Address">
-            </div>
+          <div class="form-group">
+            <label>Mobile No. </label>
+            <input type="number" name="Mobile No." class="form-control" placeholder="Enter Mobile No.">
+          </div>
+
+          <div class="form-group">
+            <label> Saloon picture </label>
+            <input type="file" name="picture" class="form-control" placeholder="Chosee Picture">
+          </div>
+
+          <div class="form-group">
+            <label> Address </label>
+            <input type="text" name="Address" class="form-control" placeholder="Enter Address">
+          </div>
         </div>
-             
+
         <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" name="registerbtn" class="btn btn-primary">Save</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" name="registerbtn" class="btn btn-primary">Save</button>
         </div>
       </form>
 
@@ -65,62 +66,69 @@ include('includes/navbar.php');
 
 <div class="container-fluid">
 
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary"> Saloon Admin Profile 
-            
-           
-    </h6>
-  </div>
+  <!-- DataTales Example -->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary"> Saloon Admin Profile
 
-  <div class="card-body">
 
-    <div class="table-responsive">
+      </h6>
+    </div>
 
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <thead>
-          <tr>
-            <th> ID </th>
-            <th>Email </th>
-            <th>Username</th>
-            <th>Password</th>
-            <th>Conform Password</th>
-            <th>Mobile No.</th>
-            <th>Saloon Name</th>
-            <th>Address </th>
-            <th>EDIT </th>
-            
-          </tr>
-        </thead>
-        <tbody>
-     
-          <tr>
-            <td> 1 </td>
-            <td> psaloon@example.com </td>
-            <td> parth Patel </td>
-            <td>abc123 </td>
-            <td>******</td>
-            <td> +91 9898989898 </td>
-            <td> Patel Saloon </td>
-            <td> Xyz,area,ahmedabad </td>
-            
-          
-            <td>
-                <form action="" method="post">
-                    <input type="hidden" name="edit_id" value="">
-                    <button  type="submit" name="edit_btn" class="btn btn-success"> EDIT</button>
+    <div class="card-body">
+
+      <div class="table-responsive">
+
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+
+          </thead>
+          <tbody>
+
+
+            <?php
+            //global $conn;
+            include('../Home/connect.php');
+            $id = $_SESSION['id'];
+            $sql = "SELECT * FROM saloon WHERE id='$id'";
+
+            $results = mysqli_query($conn, $sql);
+
+            if ($results) {
+              $rowcount = mysqli_num_rows($results);
+              if ($rowcount > 0) {
+                while ($row = mysqli_fetch_array($results)) {
+                  $saloon_id = $row['id'];
+                  $mail = $row['email'];
+                  $name = $row['username'];
+                  $phone = $row['mobile_no'];
+                  $add = $row['address'];
+                  echo "<h3><label>ID</label> : $saloon_id</h3><hr>";
+                  echo "<h3><label>Mail</label> : $mail</h3><hr>";
+                  echo "<h3><label>Username</label> : $name</h3><hr>";
+                  echo "<h3><label>Phone No</label> : $phone</h3><hr>";
+                  echo "<h3><label>Address</label> : $add</h3><hr>";
+                }
+              }
+            }
+            ?>
+
+                <form action="proupdate.php" method="post">
+                  <input type="hidden" name="edit" value="">
+                  
+                  <button type="submit" name="edit" class="btn btn-success"> EDIT</button>
+                  
                 </form>
-            </td>
-            
-          </tr>
-        
-        </tbody>
-      </table>
+              
 
+            </tr>
+         
+          </tbody>
+        </table>
+
+      </div>
     </div>
   </div>
-</div>
 
 </div>
 <!-- /.container-fluid -->
