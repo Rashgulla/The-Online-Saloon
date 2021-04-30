05998cc3ea89172a18cdc3ee2468b806afff9cf3
Sarthak0011 <sarthaksolanki11@gmail.com>
2021-04-30 16:09:58 +0530

    ok

--- saloon_admin/addservice.php
+++ saloon_admin/addservice.php
@@ -1,4 +1,28 @@
 <?php
 
-
+session_start();
+include('includes/header.php');
+include('includes/navbar.php');
 ?>
+
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+
+    <!-- Content Row -->
+
+        <!-- Content Row -->
+
+
+
+
+
+
+
+
+        <?php
+        include('includes/scripts.php');
+
+        ?>
